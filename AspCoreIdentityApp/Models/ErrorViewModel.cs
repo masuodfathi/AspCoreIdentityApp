cd0a0e3907c8e28e938193c8e3d14a380de3037d
Masoud <Masoud.fathi@gmail.com>
2023-09-15 14:55:41 +0330

    Add project files.

---
+++ AspCoreIdentityApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspCoreIdentityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
